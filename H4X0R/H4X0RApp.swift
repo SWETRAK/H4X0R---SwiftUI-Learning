e54ac8fd1fc99ad9d738101361d4e1b8aab07641
SWETRAK <kamilpietrak123@gmail.com>
2022-02-17 22:43:16 +0100

    Initial Commit

---
+++ H4X0R/H4X0RApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4X0RApp.swift
+//  H4X0R
+//
+//  Created by Kamil Pietrak on 17/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct H4X0RApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
